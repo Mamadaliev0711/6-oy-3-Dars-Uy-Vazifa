e90007479a12308743f55216907bc31383d28d51
Mamadaliev0711 <mamadaliyevsunnatullo@gmail.com>
2024-04-30 16:57:41 +0500

    vazifa bajarildi

---
+++ src/App.js
@@ -0,0 +1,147 @@
+import { useState } from "react";
+import "../src/index.css";
+import allProducts from "./data ";
+
+function App() {
+  const [products, setProducts] = useState(allProducts);
+  const [originalProducts, setOriginalProducts] = useState(allProducts);
+
+  const deleteProduct = (id) => {
+    const filteredProducts = products.filter((product) => {
+      return product.id !== id;
+    });
+    setProducts(filteredProducts);
+  };
+
+  const filterByBrand = (brand) => {
+    if (brand === "all") {
+      setProducts(originalProducts);
+    } else {
+      const filteredBrand = originalProducts.filter((product) => {
+        return product.brand === brand;
+      });
+      setProducts(filteredBrand);
+    }
+  };
+
+  const filterByPrice = (price) => {
+    const filteredPrice = originalProducts.filter((product) => {
+      return product.price <= price;
+    });
+    setProducts(filteredPrice);
+  };
+
+  const filterByRating = (rating) => {
+    const filteredRating = originalProducts.filter((product) => {
+      return product.rating >= rating;
+    });
+    setProducts(filteredRating);
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+        <h1 class="mine">Telefon Uz</h1>
+      </div>
+
+      <div className="filter-container">
+        <select
+          className="select-style"
+          onChange={(e) => {
+            filterByBrand(e.target.value);
+          }}
+        >
+          <option value="all">All</option>
+          {[...new Set(originalProducts.map((product) => product.brand))].map(
+            (brand, index) => (
+              <option key={index} value={brand}>
+                {brand}
+              </option>
+            )
+          )}
+        </select>
+
+        <select
+          className="select-style"
+          onChange={(e) => {
+            filterByPrice(parseInt(e.target.value));
+          }}
+        >
+          <option value="">Price</option>
+          <option value="100">Less than $100</option>
+          <option value="500">Less than $500</option>
+          <option value="1000">Less than $1000</option>
+        </select>
+
+        <select
+          className="select-style"
+          onChange={(e) => {
+            filterByRating(parseInt(e.target.value));
+          }}
+        >
+          <option value="">Rating</option>
+          <option value="3">At least 3 stars</option>
+          <option value="4">At least 4 stars</option>
+          <option value="5">At least 5 stars</option>
+        </select>
+      </div>
+
+      <ul className="productGrid">
+        {products.map((product) => {
+          const {
+            id,
+            thumbnail,
+            description,
+            title,
+            price,
+            discountPercentage,
+            rating,
+            brand,
+          } = product;
+          return (
+            <li key={id} className="productItem">
+              <div className="div-img">
+                {" "}
+                <img
+                  src={thumbnail}
+                  alt={description}
+                  width={400}
+                  height={100}
+                />
+              </div>
+              <div className="card-body">
+                <h2>{title}</h2>
+                <p>
+                  <b>Brand:</b> {brand}
+                </p>
+                <p>
+                  <b>Description:</b> {description}
+                </p>
+                <p>
+                  <b>Price: $</b> {price}
+                </p>
+                <p>
+                  <b>Discount :</b> {discountPercentage}%
+                </p>
+                <p>
+                  <b>Rating:</b> {rating}
+                </p>
+
+                <div className="delete-button-container">
+                  <button
+                    onClick={() => deleteProduct(id)}
+                    className="btn delete-btn"
+                  >
+                    Delete <i class="bi bi-trash"></i>
+                  </button>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
